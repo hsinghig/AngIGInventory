e7f966115d6c891746ad4625419ec6e363d7c8f1
Hemant Singh <hsingh@impact-guard.com>
2023-12-19 21:51:42 -0500

    added nav and page not found

---
+++ src/app/ui/lamination/lamination.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LaminationRoutingModule } from './lamination-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LaminationRoutingModule
+  ]
+})
+export class LaminationModule { }
